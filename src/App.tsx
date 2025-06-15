7a238e1197260101983e4792b0b296e9ce850ee3
Edem <edem.ablaev57@yandex.ru>
2025-06-15 12:35:19 +0300

    initital commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <header>
+            <h1 className="font-bold text-emerald-400 mb-4">My Web Side</h1>
+        </header>
+
+        <main>
+            Main
+            {/* <Hero></Hero> */}
+            {/* <ProjectsCarousel></ProjectsCarousel> */}
+            {/* <Contacts></Contacts> */}
+        </main>
+
+        <footer>
+            2025 Эдем Аблаев
+        </footer>
+    </>
+  )
+}
+
+export default App
