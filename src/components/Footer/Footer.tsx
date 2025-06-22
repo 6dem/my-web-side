ea69c5a439f2eb845270cebfc198d0b2e0342e62
Edem <edem.ablaev57@yandex.ru>
2025-06-22 18:16:48 +0300

    add loader activation in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,24 @@
 import { Copyright } from "lucide-react"
+import type { JSX } from "react"
+import { useLoader } from "../../context/loader"
 import cls from './Footer.module.css'
 
-const startYear = 2025
-const currentYear = new Date().getFullYear()
-const displayYear = currentYear === startYear ? `${startYear}` : `${startYear} - ${currentYear}`
+const startYear: number = 2025
+const currentYear: number = new Date().getFullYear()
+const displayYear: string = currentYear === startYear ? `${startYear}` : `${startYear} - ${currentYear}`
 
-export function Footer() {
+export function Footer(): JSX.Element {
+    const {start, stop} = useLoader()
+
+    const onLinkMouseDown = (): void => {
+        start()
+        setTimeout(stop, 1500)
+    }
+
     return (
         <footer className={cls.footer}>
             <p className={cls.copyright}>{<Copyright size={14} />} {displayYear} <span>Edem Ablaev</span></p>
-            <a href="https://github.com/6dem/my-web-side" target="_blank" rel="noopener noreferrer">Repository</a>
+            <a href="https://github.com/6dem/my-web-side" target="_blank" rel="noopener noreferrer" onMouseDown={onLinkMouseDown}>Repository</a>
         </footer>
         )
 }
